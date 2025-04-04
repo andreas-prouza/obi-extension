f05baa51f4ed7f17feb762e138353764c0795e5b
Andreas Prouza <andreas@prouza.at>
2025-04-04 17:06:50 +0200

    Extend configs

--- obi-media/.obi/etc/constants.py
+++ obi-media/.obi/etc/constants.py
@@ -3,4 +3,5 @@ CONFIG_USER_TOML = '.obi/etc/.user-app-config.toml'
 SOURCE_CONFIG_TOML = '.obi/etc/source-config.toml'
 DEPENDEND_OBJECT_LIST = '.obi/tmp/dependend-object-list.json'
 CHANGED_OBJECT_LIST = '.obi/tmp/changed-object-list.json'
+EXTENDED_SOURCE_PROCESS_CONFIG_TOML = '.obi/etc/extended-source-processing.toml'
 UPDATE_OBJECT_LIST = False
