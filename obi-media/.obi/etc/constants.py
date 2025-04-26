1370daf5c4c70b52501278e378f47fc92c7dd2c6
Andreas Prouza <andreas@prouza.at>
2025-04-26 09:30:57 +0200

    0.3.19: Scipt support in ESP

--- obi-media/.obi/etc/constants.py
+++ obi-media/.obi/etc/constants.py
@@ -4,4 +4,5 @@ SOURCE_CONFIG_TOML = '.obi/etc/source-config.toml'
 DEPENDEND_OBJECT_LIST = '.obi/tmp/dependend-object-list.json'
 CHANGED_OBJECT_LIST = '.obi/tmp/changed-object-list.json'
 EXTENDED_SOURCE_PROCESS_CONFIG_TOML = '.obi/etc/extended-source-processing-config.toml'
+ESP_SCRIPT_FOLDER='.obi/etc/scripts'
 UPDATE_OBJECT_LIST = False
