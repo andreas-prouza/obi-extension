d922a181ffdc22b35497441d6b56303e90400b76
Andreas Prouza <andreas@prouza.at>
2025-11-21 09:12:24 +0100

    Auto refresh workspace

--- src/utilities/LocalSourceList.ts
+++ src/utilities/LocalSourceList.ts
@@ -1,7 +1,10 @@
+import * as vscode from 'vscode';
 import path from "path";
 import { AppConfig } from "../webview/controller/AppConfig";
 import { Workspace } from "./Workspace";
 import { DirTool } from "./DirTool";
+import { Constants } from '../Constants';
+import { logger } from './Logger';
 
 
 export class LocalSourceList {
@@ -9,7 +12,10 @@ export class LocalSourceList {
     private static source_list: string[] | undefined = undefined;
     private static last_load_time: number = 0;
     private static source_loading_promise: Promise<void>[] = [];
-
+    
+    private static watcher_project: vscode.FileSystemWatcher | undefined = undefined;
+    private static watcher_processing_promise: Promise<void>[] = [];
+    
 
 
     private static async _load_source_list(): Promise<void> {
@@ -24,6 +30,41 @@ export class LocalSourceList {
         true
         ) || [];
         LocalSourceList.last_load_time = Date.now();
+
+        if (LocalSourceList.watcher_project === undefined) {
+            const watch_uri = vscode.Uri.joinPath(Workspace.get_workspace_uri(), config.general['source-dir'] || 'src');
+            logger.debug(`Setting up file watcher for file changes in ${watch_uri}`);
+            LocalSourceList.watcher_project = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(watch_uri, '**/*'), false, true, false);
+        
+            // File change events
+            LocalSourceList.watcher_project.onDidCreate(uri => {
+                if (LocalSourceList.watcher_processing_promise.length > 0)
+                    return;
+                LocalSourceList.source_loading_promise.push(LocalSourceList.refresh_obi_stuff(uri));
+            });
+            LocalSourceList.watcher_project?.onDidDelete(uri => {
+                if (LocalSourceList.watcher_processing_promise.length > 0)
+                    return;
+                LocalSourceList.source_loading_promise.push(LocalSourceList.refresh_obi_stuff(uri));
+            });
+        }
+    }
+
+
+    private static async refresh_obi_stuff(uri: vscode.Uri) {
+
+        const config: AppConfig = AppConfig.get_app_config();
+        const ext = uri.fsPath.split('.').pop() ?? '';
+
+        logger.debug(`File changed: ${uri.fsPath}`);
+        if (ext == 'log')
+            return;
+        if (!config.general['supported-object-types']?.includes(ext))
+            return;
+        logger.debug(`Reload some stuff`);
+        await LocalSourceList.load_source_list();
+        await vscode.commands.executeCommand("obi.source-filter.update");
+        LocalSourceList.source_loading_promise = [];
     }
 
 
